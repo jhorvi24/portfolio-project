f322a49c16bd4c9def733953e67e04dd50c1ec8c
Jhorman Villanueva <jhorvi24@hotmail.com>
2023-11-03 18:20:27 -0500

    Agregando rutas a la página principal

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/proyectos')
+def proyectos():
+    return render_template('proyectos.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0',debug=True)
