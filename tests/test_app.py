72a9aeec19b6385ec754df436f948097428ce28c
Jhorman Villanueva <jhorvi24@hotmail.com>
2023-12-04 21:09:41 -0500

    Configurando Jenkins

---
+++ tests/test_app.py
@@ -0,0 +1,8 @@
+#function for test the application
+from app import app
+
+def test_app():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    
